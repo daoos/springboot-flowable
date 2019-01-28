25a53617063bd3f0ad38139319f815325329aab3
company <1181249332@qq.com>
2019-01-28 18:06:10 +0800

    初始化flowable

---
+++ src/test/java/priv/gitonlie/flowable/SpringbootFlowableApplicationTests.java
@@ -0,0 +1,17 @@
+package priv.gitonlie.flowable;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootFlowableApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
